723c99bc37d8d259038583a56c5bd59f08171366
Adam Brown <adam.brown41@nhs.net>
2023-12-14 12:48:37 +0000

    [AEA-3656] added zipping to build script

--- build.mjs
+++ build.mjs
@@ -1,4 +1,6 @@
 import * as esbuild from "esbuild"
+import fs from "fs"
+import archiver from "archiver"
 
 const repo_packages = [
   "splunkProcessor"
@@ -15,4 +17,16 @@ for (const repo_package of repo_packages) {
     ],
     outfile: `dist/${repo_package}/app.js`,
   })
+
+  const output = fs.createWriteStream(`${repo_package}.zip`)
+  const zip = archiver("zip", { zlib: { level: 9 } })
+
+  output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+  });
+
+  zip.pipe(output)
+  zip.directory(`dist/${repo_package}/`, false)
+  zip.finalize()
 }
