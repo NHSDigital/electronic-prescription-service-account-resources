de448ba4b90ee54177cb97eefdf0d1fdac458cf6
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-01-02 17:31:51 +0000

    AEA-3656 move splunk firehose to account resources (#51)
    ## Summary
    
    **Remove items from this list if they are not relevant. Remove this line
    once this has been done**
    
    - Routine Change
    
    ### Details
    
    - new lambda for processing cloudwatch logs to splunk format. Copied
    from https://github.com/NHSDigital/prescriptionsforpatients
    - SAM template to create AWS resources needed for
    cloudwatch->firehose->splunk.
    - new stack created as part of release pipeline called lambda-resources
    that contains the above and can be used for other lambdas
    - new stack called lambda-resources-pr-xxx created as part of pull
    requests
    - change to cloudformation github action to create an empty stack if it
    does not exist
    - add the tomorrow licence as we the lambda is based off
    https://github.com/disney/terraform-aws-kinesis-firehose-splunk
    - refactor pull request and release github action to use a new action
    called release_all_stacks which releases all stacks
    - new github actions to package and release SAM templates
    - makefile targets for SAM related tasks
    - extend parse_paramaters to use environment variables to pass in
    dynamic secrets or variables which can then be passed to
    cloudformation/SAM deployments
    - changes for local environment to allow local node/SAM development
     
    ## Reviews Required
    
    **Check who should review this. Remove this line once this has been
    done**
    
    - [x] Dev
    - [ ] Test
    - [ ] Tech Author
    - [ ] Product Owner
    
    ## Review Checklist
    
    :information_source: This section is to be filled in by the
    **reviewer**.
    
    - [ ] I have reviewed the changes in this PR and they fill all or part
    of the acceptance criteria of the ticket, and the code is in a mergeable
    state.
    - [ ] I have reviewed any infrastructure changes listed in the workflow
    summary.
    - [ ] If there were infrastructure, operational, or build changes, I
    have made sure there is sufficient evidence that the changes will work.
    - [ ] I have ensured the jira ticket has been updated with the github
    pull request link
    
    ---------
    
    Co-authored-by: Adam Brown <adam.brown41@nhs.net>

--- scripts/parse_parameters.py
+++ scripts/parse_parameters.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
 import argparse
 import json
+import re
+import os
 
 EMPTY = ""
 
 
-def parse_parameters(env, stack):
+def parse_parameters(env, stack, secrets, dynamic_vars):
     """ Parse Cloudformation Parameters
     - Reads and parses json file of parameters @ cloudformation/env/<env_name>.json
-    - Takes the name of the environment and the name of the stack as arguments
+    - Takes the name of the environment, the name of the stack, and any secrets to substitute as arguments
+    - Also expect environment variables parameter_secrets and dynamic_vars to be set
+     -This is used as they may have quotes in it which are hard to pass on command line
     - Outputs "" for no parameters
     - Outputs parameters in the form 'ParameterKey=Somekey,ParameterValue=SomeValue' for create-change-set aws cli calls
     - Parameter values can be either a string for a single value,
-        or a list of strings to concatenate for multiple values
+        a list of strings to concatenate for multiple values,
+        or a string containing a reference to a secret in the format 'SECRET.<secret_name>'
     """
 
     with open(f"cloudformation/env/{env}.json") as f:
         d = json.load(f)
 
     output = EMPTY
+
+    parsed_secrets = json.loads(secrets)
+    parsed_dynamic_vars = json.loads(dynamic_vars)
 
     parameters = d.get("parameters")
     if not parameters:
@@ -28,29 +36,56 @@ def parse_parameters(env, stack):
     if not stack_parameters:
         return EMPTY
 
-    for parameter, value in stack_parameters.items():
-        if not value:
+    for parameter_key, raw_value in stack_parameters.items():
+        if not raw_value:
             return EMPTY
-        elif isinstance(value, str):
-            output = f'{output}ParameterKey="{parameter}",ParameterValue="\'{value}\'" '
-        elif isinstance(value, list):
-            values = ','.join(value)
-            output = f'{output}ParameterKey="{parameter}",ParameterValue="\'{values}\'" '
+        elif isinstance(raw_value, str):
+            value = replace_secrets(raw_value, parsed_secrets)
+            value = replace_dynamic_variables(value, parsed_dynamic_vars)
+            output = f'{output}ParameterKey="{parameter_key}",ParameterValue="\'{value}\'" '
+        elif isinstance(raw_value, list):
+            values = []
+            for list_value in raw_value:
+                value = replace_secrets(list_value, parsed_secrets)
+                value = replace_dynamic_variables(value, parsed_dynamic_vars)
+                values.append(value)
+            concatenated_values = ','.join(values)
+            output = f'{output}ParameterKey="{parameter_key}",ParameterValue="\'{concatenated_values}\'" '
         else:
             return EMPTY
 
     return output.rstrip()
 
 
+def replace_secrets(value, secrets):
+    if value.startswith("SECRET."):
+        [_, secret_name] = value.split(".")
+        real_value = secrets.get(secret_name)
+        return real_value
+    else:
+        return value
+
+
+def replace_dynamic_variables(value, dynamic_vars):
+    if value.startswith("VAR."):
+        [_, dynamic_var_name] = value.split(".")
+        real_value = dynamic_vars.get(dynamic_var_name)
+        return real_value
+    else:
+        return value
+
+
 if __name__ == "__main__":
-    """
-        Note: In github the env is split into dev-ci and dev-account, to save extra command or args in the workflow
-        we instead split the github env here to get the main env name e.g. dev
-    """
     parser = argparse.ArgumentParser()
     parser.add_argument("env", help="the environment to parse for")
     parser.add_argument("stack", help="the stack to parse for")
     args = parser.parse_args()
-    [env, _] = args.env.split("-")
 
-    print(parse_parameters(env, args.stack))
+    [env, _] = args.env.split("-")
+    regex = re.compile(r"-pr-[\d]+", re.IGNORECASE)
+    stack = regex.sub("", args.stack)
+
+    parameter_secrets = os.environ["parameter_secrets"]
+    dynamic_vars = os.environ["dynamic_vars"]
+
+    print(parse_parameters(env, stack, parameter_secrets, dynamic_vars))
