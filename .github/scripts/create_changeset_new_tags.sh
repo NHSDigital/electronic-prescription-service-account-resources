a1846c65adb0637fe3e4bab68fd661a65fd2d533
Phil Gee <originalphil@gmail.com>
2024-01-22 15:05:49 +0000

    AEA-3622 Added create_changeset_new_tags script.

---
+++ .github/scripts/create_changeset_new_tags.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ROLE=$(aws cloudformation list-exports --output json | \
+jq -r '.Exports[] | select(.Name == "ci-resources:CloudFormationExecutionRole") | .Value' )
+aws cloudformation create-change-set \
+  --stack-name "$STACK_NAME" \
+  --change-set-name "$STACK_NAME-$CHANGE_SET_VERSION" \
+  --change-set-type UPDATE \
+  --template-body "file://$TEMPLATE" \
+  --capabilities "$CAPABILITIES" \
+  --parameters "file://$PARAMETERS_FILE_NAME" \
+  --cli-binary-format raw-in-base64-out \
+  --tags "Key=\"version\",Value=\"$VERSION\"" \
+  --role-arn="$ROLE"
