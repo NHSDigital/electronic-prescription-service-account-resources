f87e979c8b22a666ebc12c2d33e8f75c57af701f
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-06-10 16:13:46 +0100

    Fix: [AEA-0000] - tests for certificateChecker handler (#172)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - create a unit tests for certificate checker handler
    - run unit tests in vscode
    - rationilse managed policies for SplunkDeliveryStreamBackupBucketRole
    - add a delay between deleting stacks to avoid 429 errors

--- packages/certificateChecker/tests/test-handler.test.ts
+++ packages/certificateChecker/tests/test-handler.test.ts
@@ -1,31 +1,52 @@
 import * as fs from "fs"
 import * as path from "path"
-import {checkCertificateExpiry} from "../src/helpers"
-import {Secret} from "../src/helpers"
+import {mockClient} from "aws-sdk-client-mock"
+import {BatchGetSecretValueCommand, SecretsManagerClient} from "@aws-sdk/client-secrets-manager"
+import {fileURLToPath} from "url"
+import {dirname} from "path"
+import {handler} from "../src/certificateChecker"
+
+import {helloworldContext} from "./helloWorldContext"
 import {Logger} from "@aws-lambda-powertools/logger"
+import {jest} from "@jest/globals"
 
-describe("checkCertificateExpiry", () => {
-  const logger = new Logger({serviceName: "test"})
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const dummyContext = helloworldContext
+const mockEvent = {
+  secretARNs: ["foo"]
+}
 
-  it("should log an info if a certificate has over 30 days left before expiry", () => {
-    const mockLoggerInfo = jest.spyOn(Logger.prototype, "info")
+describe("Unit test for app handler", function () {
+  it("Runs successfully with a valid certificate", async () => {
+    const smMock = mockClient(SecretsManagerClient)
     const validCertificateContents = fs.readFileSync(path.resolve(__dirname, "./mock-certs/valid-cert.pem"), "utf-8")
 
-    const validSecret: Secret = {
-      ARN: "valid-arn",
-      CreatedDate: new Date(),
-      Name: "valid-certificate",
-      SecretString: validCertificateContents,
-      VersionId: "valid-version-id",
-      VersionStages: ["valid-stage"]
-    }
-    checkCertificateExpiry(validSecret, logger)
+    smMock.on(BatchGetSecretValueCommand).resolves({
+      SecretValues: [
+        {
+          ARN: "valid-arn",
+          CreatedDate: new Date(),
+          Name: "valid-certificate",
+          SecretString: validCertificateContents,
+          VersionId: "valid-version-id",
+          VersionStages: ["valid-stage"]
+        }
+      ]
+    })
+    const mockLoggerInfo = jest.spyOn(Logger.prototype, "info")
+    const mockLoggerError = jest.spyOn(Logger.prototype, "error")
+    await handler(mockEvent, dummyContext)
+
+    expect(mockLoggerInfo).toHaveBeenCalled()
+    expect(mockLoggerError).not.toHaveBeenCalled()
+    expect(mockLoggerInfo).toHaveBeenCalledWith("Lambda execution started.")
+    expect(mockLoggerInfo).toHaveBeenCalledWith("Lambda execution completed.")
 
     const today = new Date()
 
     const futureDate = new Date(today)
     futureDate.setDate(today.getDate() + 60)
-
     const options: Intl.DateTimeFormatOptions = {
       weekday: "short",
       month: "short",
@@ -33,48 +54,33 @@ describe("checkCertificateExpiry", () => {
       year: "numeric"
     }
     const formattedDate = futureDate.toLocaleDateString("en-US", options)
-
     const formattedDateWithoutComma = formattedDate.replace(/,/g, "")
 
     const testString = `Certificate valid-certificate is valid. Expiry date: ${formattedDateWithoutComma}`
     expect(mockLoggerInfo).toHaveBeenCalledWith(testString)
   })
 
-  it("should log error for expiring certificate", () => {
-    const mockLoggerInfo = jest.spyOn(Logger.prototype, "error")
-    const expiringCertificateContents = fs.readFileSync(
-      path.resolve(__dirname, "./mock-certs/expiring-cert.pem"),
-      "utf-8"
-    )
-    const expiringSecret: Secret = {
-      ARN: "expiring-arn",
-      CreatedDate: new Date(),
-      Name: "expiring-certificate",
-      SecretString: expiringCertificateContents,
-      VersionId: "expiring-version-id",
-      VersionStages: ["expiring-stage"]
-    }
-    checkCertificateExpiry(expiringSecret, logger)
+  it("Log an error when something is wrong", async () => {
+    const smMock = mockClient(SecretsManagerClient)
+    const validCertificateContents = fs.readFileSync(path.resolve(__dirname, "./mock-certs/valid-cert.pem"), "utf-8")
+
+    smMock.on(BatchGetSecretValueCommand).resolves({
+      SecretValues: [
+        {
+          ARN: "valid-arn",
+          CreatedDate: new Date(),
+          Name: "valid-certificate",
+          SecretString: `aaa_${validCertificateContents}`,
+          VersionId: "valid-version-id",
+          VersionStages: ["valid-stage"]
+        }
+      ]
+    })
+    const mockLoggerInfo = jest.spyOn(Logger.prototype, "info")
+    const mockLoggerError = jest.spyOn(Logger.prototype, "error")
+    await handler(mockEvent, dummyContext)
 
     expect(mockLoggerInfo).toHaveBeenCalled()
-    expect(mockLoggerInfo).toHaveBeenCalledWith("Certificate expiring-certificate expires in 23 days")
-  })
-
-  it("should log a critical warning if a certificate is checked and has expired", () => {
-    const mockLoggerInfo = jest.spyOn(Logger.prototype, "critical")
-    const expiredCertificateInfo = fs.readFileSync(path.resolve(__dirname, "./mock-certs/expired-cert.pem"), "utf-8")
-
-    const expiredSecret: Secret = {
-      ARN: "expired-arn",
-      CreatedDate: new Date(),
-      Name: "expired-certificate",
-      SecretString: expiredCertificateInfo,
-      VersionId: "expired-version-id",
-      VersionStages: ["expired-stage"]
-    }
-    checkCertificateExpiry(expiredSecret, logger)
-
-    expect(mockLoggerInfo).toHaveBeenCalledWith("Certificate expired-certificate has expired")
-
+    expect(mockLoggerError).toHaveBeenCalled()
   })
 })
---
+++ packages/certificateChecker/tests/test-helpers.test.ts
@@ -0,0 +1,85 @@
+import * as fs from "fs"
+import * as path from "path"
+import {checkCertificateExpiry, Secret} from "../src/helpers"
+import {Logger} from "@aws-lambda-powertools/logger"
+import {jest} from "@jest/globals"
+import {fileURLToPath} from "url"
+import {dirname} from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+describe("checkCertificateExpiry", () => {
+  const logger = new Logger({serviceName: "test"})
+
+  it("should log an info if a certificate has over 30 days left before expiry", () => {
+    const mockLoggerInfo = jest.spyOn(Logger.prototype, "info")
+    const validCertificateContents = fs.readFileSync(path.resolve(__dirname, "./mock-certs/valid-cert.pem"), "utf-8")
+
+    const validSecret: Secret = {
+      ARN: "valid-arn",
+      CreatedDate: new Date(),
+      Name: "valid-certificate",
+      SecretString: validCertificateContents,
+      VersionId: "valid-version-id",
+      VersionStages: ["valid-stage"]
+    }
+    checkCertificateExpiry(validSecret, logger)
+
+    const today = new Date()
+
+    const futureDate = new Date(today)
+    futureDate.setDate(today.getDate() + 60)
+
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+      year: "numeric"
+    }
+    const formattedDate = futureDate.toLocaleDateString("en-US", options)
+
+    const formattedDateWithoutComma = formattedDate.replace(/,/g, "")
+
+    const testString = `Certificate valid-certificate is valid. Expiry date: ${formattedDateWithoutComma}`
+    expect(mockLoggerInfo).toHaveBeenCalledWith(testString)
+  })
+
+  it("should log error for expiring certificate", () => {
+    const mockLoggerInfo = jest.spyOn(Logger.prototype, "error")
+    const expiringCertificateContents = fs.readFileSync(
+      path.resolve(__dirname, "./mock-certs/expiring-cert.pem"),
+      "utf-8"
+    )
+    const expiringSecret: Secret = {
+      ARN: "expiring-arn",
+      CreatedDate: new Date(),
+      Name: "expiring-certificate",
+      SecretString: expiringCertificateContents,
+      VersionId: "expiring-version-id",
+      VersionStages: ["expiring-stage"]
+    }
+    checkCertificateExpiry(expiringSecret, logger)
+
+    expect(mockLoggerInfo).toHaveBeenCalled()
+    expect(mockLoggerInfo).toHaveBeenCalledWith("Certificate expiring-certificate expires in 23 days")
+  })
+
+  it("should log a critical warning if a certificate is checked and has expired", () => {
+    const mockLoggerInfo = jest.spyOn(Logger.prototype, "critical")
+    const expiredCertificateInfo = fs.readFileSync(path.resolve(__dirname, "./mock-certs/expired-cert.pem"), "utf-8")
+
+    const expiredSecret: Secret = {
+      ARN: "expired-arn",
+      CreatedDate: new Date(),
+      Name: "expired-certificate",
+      SecretString: expiredCertificateInfo,
+      VersionId: "expired-version-id",
+      VersionStages: ["expired-stage"]
+    }
+    checkCertificateExpiry(expiredSecret, logger)
+
+    expect(mockLoggerInfo).toHaveBeenCalledWith("Certificate expired-certificate has expired")
+
+  })
+})
