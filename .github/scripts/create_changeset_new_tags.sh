5dba8ff252c63a54a5cb91d015a2b1bfe4739906
Adam Brown <adam.brown41@nhs.net>
2024-07-04 09:34:56 +0000

    [AEA-3925] debugging pipeline

--- .github/scripts/create_changeset_new_tags.sh
+++ .github/scripts/create_changeset_new_tags.sh
@@ -2,12 +2,16 @@
 
 cd ../..
 
+echo "role"
 ROLE=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:CloudFormationExecutionRole") | .Value' )
 
 # wait for stack to finish creating or updating
+echo "waiting for create"
 aws cloudformation wait stack-create-complete --stack-name "$STACK_NAME" 
+echo "waiting for update"
 aws cloudformation wait stack-update-complete --stack-name "$STACK_NAME" 
 
+echo "create changeset"
 aws cloudformation create-change-set \
   --stack-name "$STACK_NAME" \
   --change-set-name "$STACK_NAME-$CHANGE_SET_VERSION" \
