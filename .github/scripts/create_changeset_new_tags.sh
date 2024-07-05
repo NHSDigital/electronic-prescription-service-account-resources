54c8bd8c823b1764eee3ddba40cf6d7ac6b5ceb8
Adam Brown <adam.brown41@nhs.net>
2024-07-05 13:27:04 +0000

    [AEA-3925] misc cleanup

--- .github/scripts/create_changeset_new_tags.sh
+++ .github/scripts/create_changeset_new_tags.sh
@@ -2,18 +2,14 @@
 
 cd ../..
 
-echo "role"
 ROLE=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:CloudFormationExecutionRole") | .Value' )
 
 # wait for stack to finish creating or updating
-echo "waiting for create"
 aws cloudformation wait stack-create-complete --stack-name "$STACK_NAME" 
 
 # if stack status is CREATE_COMPLETE then stack-update-complete will time out
 status=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query Stacks[0].StackStatus)
-echo "${status}"
 if [ "${status}" != '"CREATE_COMPLETE"' ]; then
-  echo "waiting for update"
   aws cloudformation wait stack-update-complete --stack-name "$STACK_NAME" 
 fi
 
