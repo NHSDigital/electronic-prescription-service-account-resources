18bf236dfdeea1c93b100d2a71ce296a61a4e94a
Adam Brown <adam.brown41@nhs.net>
2024-07-05 10:12:37 +0000

    [AEA-3925] debugging pipeline

--- .github/scripts/create_changeset_existing_tags.sh
+++ .github/scripts/create_changeset_existing_tags.sh
@@ -16,7 +16,7 @@ aws cloudformation wait stack-create-complete --stack-name "$STACK_NAME"
 # if stack status is CREATE_COMPLETE then stack-update-complete will time out
 status=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query Stacks[0].StackStatus)
 echo "$status"
-if [ "${status}" != "CREATE_COMPLETE" ]; then
+if [ "${status}" != '"CREATE_COMPLETE"' ]; then
   echo "waiting for update"
   aws cloudformation wait stack-update-complete --stack-name "$STACK_NAME" 
 fi
