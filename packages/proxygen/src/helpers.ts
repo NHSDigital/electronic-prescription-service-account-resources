81ccd8c51ff47022e2c781c9e7efd06ebf4d5a31
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-07-19 07:12:10 +0000

    create lambda to do proxygen deployment

---
+++ packages/proxygen/src/helpers.ts
@@ -0,0 +1,37 @@
+import jwt, {Secret} from "jsonwebtoken"
+import {v4 as uuidv4} from "uuid"
+import axios from "axios"
+
+const auth_url = "https://identity.prod.api.platform.nhs.uk/realms/api-producers"
+
+function createSignedJWT(privateKey: Secret) {
+  const header = {
+    typ: "JWT",
+    alg: "RS512",
+    kid: "eps-cli-key-1"
+  }
+  const jti_value = uuidv4()
+
+  const currentTimestamp = Math.floor(Date.now() / 1000)
+  const data = {
+    sub: "prescription-status-update-api-client",
+    iss: "prescription-status-update-api-client",
+    jti: jti_value,
+    aud: auth_url,
+    exp: currentTimestamp + 180 // expiry time is 180 seconds from time of creation
+  }
+
+  const signedJWT = jwt.sign(JSON.stringify(data), privateKey, {algorithm: "RS512", header: header})
+  return signedJWT
+}
+
+export async function getAccessToken(privateKey: Secret) {
+  const signedJWT = createSignedJWT(privateKey)
+  const payload = {
+    grant_type: "client_credentials",
+    client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+    client_assertion: signedJWT
+  }
+  const response = await axios.post(auth_url, payload, {headers: {"content-type": "application/x-www-form-urlencoded"}})
+  return response.data
+}
---
+++ packages/proxygen/src/proxygen.ts
@@ -0,0 +1,53 @@
+import {SecretsManagerClient, GetSecretValueCommand} from "@aws-sdk/client-secrets-manager"
+import {Logger} from "@aws-lambda-powertools/logger"
+import {injectLambdaContext} from "@aws-lambda-powertools/logger/middleware"
+import {getAccessToken} from "./helpers"
+import middy from "@middy/core"
+import inputOutputLogger from "@middy/input-output-logger"
+import axios from "axios"
+import {Secret} from "jsonwebtoken"
+
+const secretsClient = new SecretsManagerClient({})
+
+const logger = new Logger({serviceName: "certificateChecker"})
+
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+const lambdaHandler = async (event: any) => {
+  try {
+    const apiName = event.apiName
+    const environment = event.environment
+    const specDefinition = event.specDefinition
+    const instance = event.instance
+
+    const input = {
+      SecretId: "account-resources-PSU-ProxygenPrivateKey"
+    }
+    const getSecretCommand = new GetSecretValueCommand(input)
+
+    const secretResponse = await secretsClient.send(getSecretCommand)
+    const privateKey = secretResponse.SecretString
+    const accessTokenResponse = await getAccessToken(privateKey as Secret)
+    const accessToken = accessTokenResponse.access_token
+    const path = `/apis/${apiName}/environments/${environment}/instances/${instance}`
+    const response = await axios.post(path, specDefinition, {
+      headers: {"content-type": "application/json", Authorization: `Bearer: ${accessToken}`}
+    })
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const handler = middy(lambdaHandler)
+  .use(injectLambdaContext(logger, {clearState: true}))
+  .use(
+    inputOutputLogger({
+      logger: (request) => {
+        if (request.response) {
+          logger.debug(request)
+        } else {
+          logger.info(request)
+        }
+      }
+    })
+  )
