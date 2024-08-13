07d994a9b37e871f05de04b9e499abaf46253efe
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-08-13 16:52:48 +0000

    use a region

--- scripts/run_cfn_guard.sh
+++ scripts/run_cfn_guard.sh
@@ -20,9 +20,8 @@ while IFS= read -r -d '' file
 do
     echo "checking $file"
     mkdir -p "$(dirname cfn_guard_output/"$file")"
-    sam validate -t "$file" --debug 2>&1 
 
-    sam validate -t "$file" --debug 2>&1 | \
+    sam validate -t "$file" --region eu-west-2 --debug 2>&1 | \
     grep -Pazo '(?s)AWSTemplateFormatVersion.*\n\n' | \
     tr -d '\0' | \
     ~/.guard/bin/cfn-guard validate \
