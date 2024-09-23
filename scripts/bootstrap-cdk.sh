71490a73eb1325efe89146ed3747bedb0fbfd572
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-09-23 07:52:23 +0000

    add bootstrap-cdk script

---
+++ scripts/bootstrap-cdk.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# get the policy arns
+GrantCloudFormationExecutionAccessIAMPolicy=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:GrantCloudFormationExecutionAccessIAMPolicy") | .Value' | grep -o '[^:]*$')
+GrantCloudFormationExecutionAccessPolicyA=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:GrantCloudFormationExecutionAccessPolicyA") | .Value' | grep -o '[^:]*$')
+GrantCloudFormationExecutionAccessPolicyB=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:GrantCloudFormationExecutionAccessPolicyB") | .Value' | grep -o '[^:]*$')
+GrantCloudFormationExecutionAccessPolicyC=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:GrantCloudFormationExecutionAccessPolicyC") | .Value' | grep -o '[^:]*$')
+GrantCloudFormationExecutionAccessPolicyD=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "ci-resources:GrantCloudFormationExecutionAccessPolicyD") | .Value' | grep -o '[^:]*$')
+
+# get the current aws account id
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+ALL_POLICIES=${GrantCloudFormationExecutionAccessIAMPolicy}
+ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyA}"
+ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyB}"
+ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyC}"
+ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyD}"
+
+
+echo "Going to run cdk bootstrap with these parameters"
+echo "GrantCloudFormationExecutionAccessIAMPolicy: ${GrantCloudFormationExecutionAccessIAMPolicy}"
+echo "GrantCloudFormationExecutionAccessPolicyA: ${GrantCloudFormationExecutionAccessPolicyA}"
+echo "GrantCloudFormationExecutionAccessPolicyB: ${GrantCloudFormationExecutionAccessPolicyB}"
+echo "GrantCloudFormationExecutionAccessPolicyC: ${GrantCloudFormationExecutionAccessPolicyC}"
+echo "GrantCloudFormationExecutionAccessPolicyD: ${GrantCloudFormationExecutionAccessPolicyD}"
+echo "ALL_POLICIES: ${ALL_POLICIES}"
+echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
+
+read -p "Press any key to resume or press ctrl+c to exit ..."
+
+cdk bootstrap aws://${AWS_ACCOUNT_ID}/eu-west-2 \
+  --cloudformation-execution-policies "${ALL_POLICIES}" 
+
+cdk bootstrap aws://${AWS_ACCOUNT_ID}/us-east-1 \
+  --cloudformation-execution-policies "${ALL_POLICIES}" 
