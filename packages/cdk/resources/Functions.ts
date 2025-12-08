e75c85e44da90bc463628fd5b881f131aced993b
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2025-12-08 14:07:00 +0000

    Fix: [AEA-0000] - use decrypt kms policy on supression reporter lambda (#1790)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - use decrypt kms policy on supression reporter lambda

--- packages/cdk/resources/Functions.ts
+++ packages/cdk/resources/Functions.ts
@@ -2,7 +2,7 @@ import {Construct} from "constructs"
 import {LayerVersion, Runtime} from "aws-cdk-lib/aws-lambda"
 import {IStringParameter} from "aws-cdk-lib/aws-ssm"
 import {ManagedPolicy, PolicyStatement} from "aws-cdk-lib/aws-iam"
-import {Stack} from "aws-cdk-lib"
+import {Fn, Stack} from "aws-cdk-lib"
 import {TypescriptLambdaFunction} from "@nhsdigital/eps-cdk-constructs"
 import {resolve} from "path"
 
@@ -22,11 +22,18 @@ export class Functions extends Construct {
     super(scope, id)
 
     // Imports
+    const decryptSecretsKMSKeyPolicyImport = Fn.importValue("account-resources:LambdaDecryptSecretsKMSPolicy")
     const parameterAndSecretsLayerArn =
       "arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:20"
     const parameterAndSecretsLayer = LayerVersion.fromLayerVersionArn(
       this, "LayerFromArn", parameterAndSecretsLayerArn)
 
+    // Coerce context and imports to relevant types
+    const decryptSecretsKMSKeyPolicy = ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "DecryptSecretsKMSKeyPolicyImport",
+      decryptSecretsKMSKeyPolicyImport
+    )
     // Resources
 
     const readAlertSuppressionsPolicy = new ManagedPolicy(this, "ReadAlertSuppressionsPolicy", {
@@ -60,7 +67,7 @@ export class Functions extends Construct {
       environmentVariables: {
         PARAMETERS_SECRETS_EXTENSION_HTTP_PORT: "2773"
       },
-      additionalPolicies: [readAlertSuppressionsPolicy],
+      additionalPolicies: [readAlertSuppressionsPolicy, decryptSecretsKMSKeyPolicy],
       logRetentionInDays: props.logRetentionInDays,
       logLevel: props.logLevel,
       version: props.version,
