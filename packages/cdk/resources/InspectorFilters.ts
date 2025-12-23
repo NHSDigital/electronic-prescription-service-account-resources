609881328d49958439c025d54f4a7e02fecdea06
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2025-12-23 11:32:48 +0000

    Fix: [AEA-0000] - add asdf go stdlib suppression (#1836)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - add asdf go stdlib suppression

--- packages/cdk/resources/InspectorFilters.ts
+++ packages/cdk/resources/InspectorFilters.ts
@@ -59,8 +59,10 @@ export class InspectorFilters extends Construct {
       // from go stdlib used in asdf, not applicable
       "CVE-2025-58187", // https://nvd.nist.gov/vuln/detail/CVE-2025-58187
       // from go stdlib used in sam cli, not applicable
-      "CVE-2025-64756" // https://nvd.nist.gov/vuln/detail/CVE-2025-64756
+      "CVE-2025-64756", // https://nvd.nist.gov/vuln/detail/CVE-2025-64756
       // suppression of vulnerability in glob shell - we dont use this so fine to suppress
+      "CVE-2025-61729" // https://nvd.nist.gov/vuln/detail/CVE-2025-61729
+      // suppression of vulnerability in golang stdlib - imported from asdf. Fine to suppress
     ]
 
     for (const cve of cvesToSuppress){
