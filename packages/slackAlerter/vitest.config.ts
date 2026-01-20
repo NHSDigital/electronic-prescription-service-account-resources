ab8bf6ddcee13e783d9146ac6197720d6fc005c3
Anthony Brown <anthony.brown8@nhs.net>
2026-01-20 20:44:14 +0000

    remove more jest

--- packages/slackAlerter/vitest.config.ts
+++ packages/slackAlerter/vitest.config.ts
@@ -5,6 +5,10 @@ export default mergeConfig(sharedVitestConfig, defineConfig({
   test: {
     include: ["tests/**/*.test.ts"],
     exclude: ["**/lib/**", "**/node_modules/**"],
-    setupFiles: ["./.jest/setEnvVars.js"]
+    env: {
+      "ENV": "dev",
+      "AWS_SESSION_TOKEN": "76b6033a-232c-4b5c-8d92-39760202b2d8",
+      "PARAMETERS_SECRETS_EXTENSION_HTTP_PORT":  "2773"
+    }
   }
 }))
